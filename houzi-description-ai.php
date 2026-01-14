c718a9884646455bb489bc306eb4522622b19c56
Adil Soomro <adilsoomro.s@gmail.com>
2026-01-14 21:02:50 +0500

    fixed url.

--- houzi-description-ai.php
+++ houzi-description-ai.php
@@ -8,7 +8,7 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              https://https://booleanbites.com
+ * @link              https://booleanbites.com
  * @since             1.0.0
  * @package           Houzi_Description_Ai
  *
@@ -18,7 +18,7 @@
  * Description:       A plugin that helps writing description with AI for properties, agents, agencies.
  * Version:           1.0.0
  * Author:            BooleanBites Ltd
- * Author URI:        https://https://booleanbites.com/
+ * Author URI:        https://booleanbites.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       houzi-description-ai
@@ -26,7 +26,7 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if (!defined('WPINC')) {
 	die;
 }
 
@@ -35,14 +35,15 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'HOUZI_DESCRIPTION_AI_VERSION', '1.0.0' );
+define('HOUZI_DESCRIPTION_AI_VERSION', '1.0.0');
 
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-houzi-description-ai-activator.php
  */
-function activate_houzi_description_ai() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-houzi-description-ai-activator.php';
+function activate_houzi_description_ai()
+{
+	require_once plugin_dir_path(__FILE__) . 'includes/class-houzi-description-ai-activator.php';
 	Houzi_Description_Ai_Activator::activate();
 }
 
@@ -50,19 +51,20 @@ function activate_houzi_description_ai() {
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-houzi-description-ai-deactivator.php
  */
-function deactivate_houzi_description_ai() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-houzi-description-ai-deactivator.php';
+function deactivate_houzi_description_ai()
+{
+	require_once plugin_dir_path(__FILE__) . 'includes/class-houzi-description-ai-deactivator.php';
 	Houzi_Description_Ai_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_houzi_description_ai' );
-register_deactivation_hook( __FILE__, 'deactivate_houzi_description_ai' );
+register_activation_hook(__FILE__, 'activate_houzi_description_ai');
+register_deactivation_hook(__FILE__, 'deactivate_houzi_description_ai');
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-houzi-description-ai.php';
+require plugin_dir_path(__FILE__) . 'includes/class-houzi-description-ai.php';
 
 /**
  * Begins execution of the plugin.
@@ -73,7 +75,8 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-houzi-description-ai.php';
  *
  * @since    1.0.0
  */
-function run_houzi_description_ai() {
+function run_houzi_description_ai()
+{
 
 	$plugin = new Houzi_Description_Ai();
 	$plugin->run();
